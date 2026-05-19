720e818a0573ba7b6b1671cf2e40fb1ca7f6aa4d
Chris McNutt <ctmcnutt1@gmail.com>
2026-05-19 16:23:10 -0400

    PS-3: Shadcn ui library configured in project, example button using the library added to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-3xl font-semibold">Portfolio Site</h1>
+      <Button>Portfolio Site</Button>
     </main>
   );
 }
